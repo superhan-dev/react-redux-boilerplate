8f75a039344f0e57f77cf9b9e59fd54af14e2964
superhan-dev <superhan.dev@gmail.com>
2021-08-16 16:43:45 +0900

    user api init

---
+++ src/_apis/user.api.js
@@ -0,0 +1,35 @@
+
+import BaseApi from "./base.api";
+
+export const UserApi = {
+  authenticate,
+  logout,
+  getMyProfile,
+  updateMyProfile,
+  updatePassword
+}
+
+function authenticate(username, password){
+  return BaseApi.post('/authenticate', {username: username, password: password});
+}
+
+function logout(){
+  return BaseApi.get('/logout');
+}
+
+function getMyProfile(){
+  return BaseApi.get('/user/myprofile');
+}
+
+function updateMyProfile(formData){
+  return BaseApi.post('/user/web/myprofile', formData, { headers: {
+      'Content-Type' : 'multipart/form-data'
+  }})
+}
+
+function updatePassword(currentPassword, updatedPassword){
+  return BaseApi.post('/user/password', {currentPassword: currentPassword, 
+      updatedPassword: updatedPassword});
+}
+
+
